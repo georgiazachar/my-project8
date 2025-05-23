08168b7c9bd440c8b35d9f29feb43cbbd73239ef
georgiazachar <georgiazachariadou@yahoo.com>
2025-05-23 16:22:08 +0200

    feat: add gitignore, readme and hello world class

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,23 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Εμφανίζει τη φράση Hello World στην κονσόλα
+ * (standard output)
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        /*
+         * Block Comments
+         *
+         */
+        System.out.print("Hello CF8!"); // Εκτυπώνει Hello CF8
+        System.out.println();
+
+        // Εκτυπώνει Hello Coding
+        System.out.print("Hello ");
+        System.out.print("Coding!");
+    }
+}
